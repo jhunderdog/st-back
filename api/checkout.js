debcfbb8974c62553a90b69addaa244933237d7b
jhunderdog <33382water@gmail.com>
2021-11-06 14:15:02 +0900

    webhook

--- api/checkout.js
+++ api/checkout.js
@@ -3,6 +3,8 @@ const stripeAPI = require('../stripe');
 async function createCheckoutSession(req, res) {
   const domainUrl = process.env.WEB_APP_URL;
   const { line_items, customer_email } = req.body;
+  console.log(line_items)
+  console.log(customer_email)
   // check req body has line items and email
   if (!line_items || !customer_email) {
     return res.status(400).json({ error: 'missing required session parameters' });
