5cd03c8a0263e75fa50d2a5d06160e5a23e6b2b2
jhunderdog <33382water@gmail.com>
2021-11-06 22:17:45 +0900

    payment intent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config({ path: './.env' });
 const createCheckoutSession = require('./api/checkout');
 const webhook = require('./api/webhook');
+const paymentIntent = require('./api/paymentIntent');
 const app = express();
 const port = 8080;
 
@@ -15,6 +16,7 @@ app.use(cors({ origin: true }));
 app.get('/', (req, res)=> res.send('Hello World'));
 
 app.post('/create-checkout-session', createCheckoutSession);
+app.post('/create-payment-intent', paymentIntent);
 app.post('/webhook', webhook);
 
 app.listen(port, () => { console.log('server listening on port', port)})
