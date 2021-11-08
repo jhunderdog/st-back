f1870d545a5d8413992157288593fd2a068ecfce
jhunderdog <33382water@gmail.com>
2021-11-08 19:52:10 +0900

    create get payment method endpoint

--- api/paymentIntent.js
+++ api/paymentIntent.js
@@ -19,7 +19,7 @@ async function paymentIntent(req, res) {
             shipping,
         });
         res.status(200).json({
-            clientSecret: paymentIntent.client_secret,
+            clientSecret: paymentIntent.client_secret, id: paymentIntent.id
         })
     } catch(error){
         console.log(error);
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ require('dotenv').config({ path: './.env' });
 const createCheckoutSession = require('./api/checkout');
 const webhook = require('./api/webhook');
 const paymentIntent = require('./api/paymentIntent');
+const decodeJWT = require('./auth/decodeJWT');
+const setupIntent = require('./api/setupIntent');
+const validateUser = require('./auth/validateUser');
+const getCards = require('./api/getPaymentMethod');
+const updatePaymentIntent = require('./api/updatePaymentIntent');
+
 const app = express();
 const port = 8080;
 
@@ -12,11 +18,19 @@ app.use(express.json({
 }));
 
 app.use(cors({ origin: true }));
+app.use(decodeJWT);
 
 app.get('/', (req, res)=> res.send('Hello World'));
 
 app.post('/create-checkout-session', createCheckoutSession);
 app.post('/create-payment-intent', paymentIntent);
+
+app.post('/save-payment-method', validateUser, setupIntent);
+
+app.get('/get-payment-methods', validateUser, getCards);
+
+app.put('/update-payment-intent', validateUser, updatePaymentIntent);
+
 app.post('/webhook', webhook);
 
 app.listen(port, () => { console.log('server listening on port', port)})
